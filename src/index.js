2b4c037e1914224ab57a42e7037eafeaa6c65a96
אור גנון <organon333@Gmail.com>
2022-06-16 19:46:29 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+function MyCompoment()
+{
+  return <h1>my first component</h1>
+}
+//Create the root div for REACT
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <MyCompoment />
+      
+  // <React.StrictMode>
+    
+  // </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
